67decb9b378d073ce6a663924bad7f98fa5f7dfb
Jeremy Ng Kwik Tung <s3842858@student.rmit.edu.au>
2021-10-05 00:31:30 +1100

    Order Page

--- order/index.php
+++ order/index.php
@@ -3,81 +3,29 @@
 <?php require_once('../crud/productCrud.php'); ?>
 <?php require_once('../crud/userCrud.php'); ?>
 <?php require_once('../includes/authorise.inc.php'); ?>
+<?php require_once('../includes/functions.inc.php'); ?>
 <?php require_once('../model/user.php'); ?>
 <?php require_once('../model/order.php'); ?>
 <?php
-if (isset($_GET["order_id"])) {
+if (isset($_GET["order_id"]) && isset($_SESSION[DES_SESSION_KEY])) {
     // TODO DATE CONVERSION FOR ORDER
+    $currentUser = getLoggedInUser();
+    $encryptedUsername = encryptData($currentUser->getUsername(), $_SESSION[CLIENT_PUBLIC_KEY]);
+    $encryptedSessionKey = encryptData($_SESSION[DES_SESSION_KEY], $_SESSION[CLIENT_PUBLIC_KEY]);
     openssl_private_decrypt(base64_decode(str_replace(' ', '+', $_GET["order_id"])), $orderId, $_SESSION[PRIVATE_KEY]);
     $orderCrud = new OrderCrud();
     $cartItemCrud = new CartItemCrud();
     $productCrud = new ProductCrud();
     $order = $orderCrud->readOne($orderId);
+    $encryptedOrderId = encryptData($order->getOrderId(), $_SESSION[CLIENT_PUBLIC_KEY]);
+    $encryptedOrderTotal = encryptData($order->getTotal(), $_SESSION[CLIENT_PUBLIC_KEY]);
     $cartItems = $cartItemCrud->getAllCartItemFromCartId($order->getCartId());
     $products = $productCrud->readCartItems($cartItems);
-
-    // EXAMPLE PRODUCT DIV
-    /*
-                    <div class="product">
-                    <div class="product__image-div">
-                        <img class="product__image" src="../images/horizon_forbidden_west_ps5.PNG">
-                    </div>
-                    <div class="product__card">
-                        <div class="product__row">
-                            <div class="product__name">
-                                Horizon Forbidden West PS5 Game
-                            </div>
-                            <div class="product__price">
-                                $69.95
-                            </div>
-                        </div>
-                        <div class="product__quantity">
-                            Quantity: 1
-                        </div>
-                        <div class="product__manufacturer">
-                            Playstation Studios
-                        </div>
-                        <div class="product__category">
-                            PS5
-                        </div>
-                    </div>
-                </div>
-    */
-
-    $productDivArray = [];
-    foreach ($products as $product) {
-        array_push($productDivArray, '
-        <div class="product">
-            <div class="product__image-div">
-                <img class="product__image" src="../images/' . $product->getImage() . '">
-            </div>
-            <div class="product__card">
-            <div class="product__row">
-                <div class="product__name">
-                    ' . $product->getName() . '
-                </div>
-                <div class="product__price">
-                    $' . number_format($product->getPrice(), 2) .  '
-                </div>
-            </div>
-            <div class="product__quantity">
-                Quantity: ' . $cartItems[$product->getProductId()]->getQuantity() . '
-            </div>
-            <div class="product__manufacturer">
-                ' . $product->getManufacturer() . '
-            </div>
-            <div class="product__category">
-                ' . $product->getCategory() . '
-            </div>
-        </div>
-        </div>
-        ');
-    }
 } else {
     header('location:/SEC-Practical-Assignment/');
 }
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -86,20 +34,59 @@ if (isset($_GET["order_id"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="../images/app_icon.png">
     <link rel="stylesheet" type="text/css" href="../styles/order.css">
+    <link rel="stylesheet" type="text/css" href="../styles/product.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script type="text/javascript" src="../scripts/sha256.js"></script>
+    <script type="text/javascript" src="../scripts/rsa.js"></script>
+    <script type="text/javascript" src="../scripts/functions.js"></script>
 </head>
 
 <body class="content">
-    <header>
-        <!-- Put website header here -->
-    </header>
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <!--Logo on the left side-->
+                <div class="logo">
+                    <a href="../">
+                        <img src="../images/app_icon.png" width="125px">
+                    </a>
+                </div>
+
+                <!--navbar links-->
+                <nav>
+                    <ul>
+                        <li style="color: #000000">Welcome '<span id="usernameOrder"></span>'!</li>
+                        <li><a href="../">Home</a></li>
+                        <li><a href="">Account</a></li>
+                        <li>
+                            <form id="logoutForm" action="../logout.php" method="post">
+                                <input name="logout" id="logout" type="submit" value="Logout" onclick="removePrivatePublicKey()">
+                            </form>
+                        </li>
+                    </ul>
+                </nav>
+
+                <!--Cart image-->
+                <a href="../shoppingcart/">
+                    <img src="../images/cart.png" width="30px" height="30px">
+                </a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
     <h1>Thank you! Your order has been received.</h1>
+    <br><br>
     <article>
         <div class="order">
-            <h2>Order #<?php echo $order->getOrderId() ?> - Steven Harjapramana</h2>
+            <h2>Order #<span id="order-id-top"></span></h2>
+            <br>
             <div class="order-summary-div">
-                <h1 class="text-uppercase">Order Confirmation</h1>
+                <h1 class="text-uppercase">Order Confirmation - <span id="usernameOrderSummary"></span></h1>
+                <br>
                 <div>
                     <img src="../images/order-confirmation.svg" class="order-confirmation-img">
+                    <br><br>
                     <p>
                         Thank you for your order! The product(s) will be delivered to you once we have fully processed the payment.
                     </p>
@@ -114,65 +101,54 @@ if (isset($_GET["order_id"])) {
                         <th>Payment Method</th>
                     </thead>
                     <tbody>
-                        <td>#<?php echo $order->getOrderId() ?></td>
+                        <td>#<span id="order-id-bottom"></span></td>
                         <td><?php echo date('d M Y', $order->getTimestamp()) ?></td>
-                        <td>$<?php echo number_format($order->getTotal(), 2) ?></td>
+                        <td>$<span id="order-total-top"></span></td>
                         <td>Credit Card</td>
                     </tbody>
                 </table>
             </div>
             <div class="product-list">
-                <!-- <div class="product">
-                    <div class="product__image-div">
-                        <img class="product__image" src="../images/horizon_forbidden_west_ps5.PNG">
-                    </div>
-                    <div class="product__card">
-                        <div class="product__row">
-                            <div class="product__name">
-                                Horizon Forbidden West PS5 Game
+                <?php $i = 0; ?>
+                <?php foreach ($products as $product) {
+                    $encryptedProductName = encryptData($product->getName(), $_SESSION[CLIENT_PUBLIC_KEY]);
+                    $encryptedProductId = encryptData($product->getProductId(), $_SESSION[CLIENT_PUBLIC_KEY]);
+                    $encryptedProductPrice = encryptData($product->getPrice(), $_SESSION[CLIENT_PUBLIC_KEY]);
+                    $encryptedProductImage = encryptData($product->getImage(), $_SESSION[CLIENT_PUBLIC_KEY]);
+                    $encryptedItemQuantity = encryptData($cartItems[$product->getProductId()]->getQuantity(), $_SESSION[CLIENT_PUBLIC_KEY]);
+                    $encryptedProductManufacturer = encryptData($product->getManufacturer(), $_SESSION[CLIENT_PUBLIC_KEY]);
+                    $encryptedProductCategory = encryptData($product->getCategory(), $_SESSION[CLIENT_PUBLIC_KEY]);
+                ?>
+                    <div class="product">
+                        <div class="product__image-div">
+                            <img class="product__image" id="product__image-<?php echo $i; ?>" src="" />
+                        </div>
+                        <div class="product__card">
+                            <div class="product__row">
+                                <div class="product__name" id="product__name-<?php echo $i; ?>">
+                                </div>
+                                <div class="product__price" id="product__price-<?php echo $i; ?>">
+                                </div>
                             </div>
-                            <div class="product__price">
-                                $69.95
+                            <div class="product__quantity">
+                                Quantity: <span id="product__quantity-<?php echo $i; ?>"></span>
                             </div>
-                        </div>
-                        <div class="product__quantity">
-                            Quantity: 1
-                        </div>
-                        <div class="product__manufacturer">
-                            Playstation Studios
-                        </div>
-                        <div class="product__category">
-                            PS5
-                        </div>
-                    </div>
-                </div>
-                <div class="product">
-                    <div class="product__image-div">
-                        <img class="product__image" src="../images/horizon_forbidden_west_ps5.PNG">
-                    </div>
-                    <div class="product__card">
-                        <div class="product__row">
-                            <div class="product__name">
-                                Horizon Forbidden West PS5 Game
+                            <div class="product__manufacturer" id="product__manufacturer-<?php echo $i; ?>">
                             </div>
-                            <div class="product__price">
-                                $69.95
+                            <div class="product__category" id="product__category-<?php echo $i; ?>">
                             </div>
                         </div>
-                        <div class="product__quantity">
-                            Quantity: 1
-                        </div>
-                        <div class="product__manufacturer">
-                            Manufacturer: Playstation Studios
-                        </div>
-                        <div class="product__category">
-                            Category: PS5
-                        </div>
                     </div>
-                </div> -->
-                <?php foreach ($productDivArray as $productToDisplay) {
-                    echo $productToDisplay;
-                } ?>
+                    <script>
+                        decryptImage('product__image-<?php echo $i; ?>', '<?php echo $encryptedProductImage; ?>', '../images/');
+                        decryptData('product__name-<?php echo $i; ?>', '<?php echo $encryptedProductName; ?>', '');
+                        decryptData('product__price-<?php echo $i; ?>', '<?php echo $encryptedProductPrice; ?>', '$');
+                        decryptData('product__quantity-<?php echo $i; ?>', '<?php echo $encryptedItemQuantity; ?>', '');
+                        decryptData('product__manufacturer-<?php echo $i; ?>', '<?php echo $encryptedProductManufacturer; ?>', '');
+                        decryptData('product__category-<?php echo $i; ?>', '<?php echo $encryptedProductCategory; ?>', '');
+                    </script>
+                    <?php $i++; ?>
+                <?php } ?>
             </div>
             <div class="flex-center customer">
                 <div class="customer__address">
@@ -186,7 +162,7 @@ if (isset($_GET["order_id"])) {
                 <div class="total">
                     <div class="total__item flex-between flex-align-center">
                         <div class="total__label">Total</div>
-                        <div class="total__value">$<?php echo number_format($order->getTotal(), 2) ?></div>
+                        <div class="total__value">$<span id="order-total-bottom"></span></div>
                     </div>
                     <form id="logoutForm" action="/SEC-Practical-Assignment/logout.php" method="post" class="cta-form flex-center flex-align-center">
                         <!-- <input name="logout" id="logout" type="submit" value="Logout" class="btn"> -->
@@ -195,11 +171,19 @@ if (isset($_GET["order_id"])) {
                 </div>
             </div>
         </div>
-
     </article>
+    <br>
     <footer>
         <p>&copy; 2021 Jeremy Ng Kwik Tung, Steven Harja, Wilbert Ongosari & Aman Khan</p>
     </footer>
 </body>
+<script>
+    decryptData('usernameOrder', '<?php echo $encryptedUsername; ?>', '');
+    decryptData('usernameOrderSummary', '<?php echo $encryptedUsername; ?>', '');
+    decryptData('order-id-top', '<?php echo $encryptedOrderId; ?>', '');
+    decryptData('order-id-bottom', '<?php echo $encryptedOrderId; ?>', '');
+    decryptData('order-total-top', '<?php echo $encryptedOrderTotal ?>', '');
+    decryptData('order-total-bottom', '<?php echo $encryptedOrderTotal ?>', '');
+</script>
 
 </html>
