983f70963a144ef8d7543326ba9c04d1376f1344
Wilbert Ongosari <s3512471@student.rmit.edu.au>
2021-10-05 21:07:07 +1100

    Update Account page skeleton

--- account/index.php
+++ account/index.php
@@ -1,3 +1,22 @@
+<?php require_once('../crud/userCrud.php'); ?>
+<?php require_once('../includes/authorise.inc.php'); ?>
+<?php require_once('../includes/functions.inc.php'); ?>
+<?php require_once('../model/user.php'); ?>
+
+<?php
+    // $_SESSION[DES_SESSION_KEY] = 'kjeRweiuhTawehweFLiawEh';
+    if (isset($_SESSION[DES_SESSION_KEY])) {
+        $currentUser = getLoggedInUser();
+        $encryptedUsername = encryptData($currentUser->getUsername(), $_SESSION[CLIENT_PUBLIC_KEY]);
+        $encryptedSessionKey = encryptData($_SESSION[DES_SESSION_KEY], $_SESSION[CLIENT_PUBLIC_KEY]);
+        //print_r($currentUser);
+    } else {
+        header('Location: logout.php');
+        exit();
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -41,6 +60,43 @@
             </div>
         </div>
     </div>
+
+    <div class="acc-container">
+        <div class="small-container">
+            <form action="index.php" method="POST">
+                <fieldset class="fieldset">
+                    <h2>Your Account Details</h2> <br>
+
+                    <h3><b>Username:</b></h3>
+                    <?php echo $currentUser->getUsername()?>
+                    <!--<input type="text" name="username"> <br>-->
+
+                    <br><br>
+                    <h3><b>Password:</b></h3>
+                    <input type="text" name="password"> <br>
+
+                    <a href="">
+                        <p>Change Password?</p>
+                    </a> <br>
+                    
+                    <h3><b>Firstname:</b></h3>
+                    <input type="text" name="fname"> <br><br>
+
+                    <h3><b>Lastname:</b></h3>
+                    <input type="text" name="lname"> <br><br>
+
+                    <h3><b>Date of Birth:</b></h3>
+                    <input type="text" name="dob">
+                    <br><br>
+                    <h3><b>Shipping address:</b></h3>
+                    <input type="text" name="shipAddress"> <br><br>
+
+                    <input type="submit" value="Update Details">
+                </fieldset>
+            </form>
+        </div>
+    </div>
+
 </body>
 
 </html>
